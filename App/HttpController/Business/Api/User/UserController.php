a5e649f856d5d5cca55c615427ba74f53802bd0a
wanghan <minglongoc@me.com>
2020-10-29 16:28:02 +0800

    2020-10-29_16:28:02

--- App/HttpController/Business/Api/User/UserController.php
+++ App/HttpController/Business/Api/User/UserController.php
@@ -4,6 +4,7 @@ namespace App\HttpController\Business\Api\User;
 
 use App\HttpController\Business\BusinessBase;
 use App\HttpController\Service\CreateTable;
+use wanghanwanghan\someUtils\control;
 
 class UserController extends BusinessBase
 {
@@ -15,8 +16,8 @@ class UserController extends BusinessBase
     //用户注册
     function reg()
     {
-        CreateTable::getInstance()->miniapp_ent_type();
-        var_dump(123123);
+        CreateTable::getInstance()->miniapp_ent_tax_type();
+        $this->writeJson(200,control::getUuid());
     }
 
     //用户登录
--- App/HttpController/Service/CreateTable.php
+++ App/HttpController/Service/CreateTable.php
@@ -42,12 +42,33 @@ class CreateTable extends ServiceBase
         return 'ok';
     }
 
+    //公司类型表
     function miniapp_ent_type()
     {
         $sql = DDLBuilder::table(__FUNCTION__, function (Table $table) {
             $table->setTableComment('公司类型表')->setTableEngine(Engine::INNODB)->setTableCharset(Character::UTF8MB4_GENERAL_CI);
             $table->colInt('id', 11)->setIsAutoIncrement()->setIsUnsigned()->setIsPrimaryKey()->setColumnComment('主键');
             $table->colVarChar('entType', 100)->setDefaultValue('')->setColumnComment('公司类型');
+            $table->colInt('created_at', 11)->setIsUnsigned()->setDefaultValue(0);
+            $table->colInt('updated_at', 11)->setIsUnsigned()->setDefaultValue(0);
+        });
+
+        $obj = Manager::getInstance()->get('miniapp')->getObj();
+
+        $obj->rawQuery($sql);
+
+        Manager::getInstance()->get('miniapp')->recycleObj($obj);
+
+        return 'ok';
+    }
+
+    //公司纳税类型表
+    function miniapp_ent_tax_type()
+    {
+        $sql = DDLBuilder::table(__FUNCTION__, function (Table $table) {
+            $table->setTableComment('公司纳税类型表')->setTableEngine(Engine::INNODB)->setTableCharset(Character::UTF8MB4_GENERAL_CI);
+            $table->colInt('id', 11)->setIsAutoIncrement()->setIsUnsigned()->setIsPrimaryKey()->setColumnComment('主键');
+            $table->colVarChar('entTaxType', 100)->setDefaultValue('')->setColumnComment('公司纳税类型');
             $table->colInt('created_at', 11)->setIsUnsigned()->setDefaultValue(0);
             $table->colInt('updated_at', 11)->setIsUnsigned()->setDefaultValue(0);
         });
