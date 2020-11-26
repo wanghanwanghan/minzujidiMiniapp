c217a96b3241cbe4e54a48a74d6c946943ea8498
wanghan <minglongoc@me.com>
2020-11-26 13:06:36 +0800

    2020-11-26_13:06:36

--- App/HttpController/Service/Pay/wx/wxPayService.php
+++ App/HttpController/Service/Pay/wx/wxPayService.php
@@ -14,6 +14,7 @@ class wxPayService
     {
         $conf = new wxConf();
 
+        $conf->setAppId('wx864577e52e8277a2');
         $conf->setMiniAppId('wx864577e52e8277a2');
         $conf->setMchId('1603847607');
         $conf->setKey('qqaazzwwssxxeeddccrrffvvttggbbyy');
