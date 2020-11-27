870c5ac056facfc3fe1db2b767663876eefa321a
wanghan <minglongoc@me.com>
2020-11-27 15:06:26 +0800

    2020-11-27_15:06:26

---
+++ produce.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'SERVER_NAME' => "minzujidiMiniapp",
+    'MAIN_SERVER' => [
+        'LISTEN_ADDRESS' => '0.0.0.0',
+        'PORT' => 8991,
+        'SERVER_TYPE' => EASYSWOOLE_WEB_SOCKET_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
+        'SOCK_TYPE' => SWOOLE_TCP,
+        'RUN_MODEL' => SWOOLE_PROCESS,
+        'SETTING' => [
+            'worker_num' => 8,
+            'reload_async' => true,
+            'max_wait_time' => 3,
+            'dispatch_mode' => 2,
+            'package_max_length' => 50 * 1024 * 1024,
+        ],
+        'TASK' => [
+            'workerNum' => 4,
+            'maxRunningNum' => 128,
+            'timeout' => 15
+        ]
+    ],
+    'TEMP_DIR' => null,
+    'LOG_DIR' => null
+];
