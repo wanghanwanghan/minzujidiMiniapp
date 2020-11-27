c3268c35c733b7b4979481f7834ac6ee09ff58d3
wanghan <minglongoc@me.com>
2020-11-27 15:17:36 +0800

    2020-11-27_15:17:36

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -5,6 +5,8 @@ namespace EasySwoole\EasySwoole;
 use App\HttpController\Service\CreateMysqlOrm;
 use App\HttpController\Service\CreateMysqlPool;
 use App\HttpController\Service\CreateRedisPool;
+use EasySwoole\Socket\Dispatcher;
+use App\WebSocket\WebSocketParser;
 use EasySwoole\EasySwoole\Swoole\EventRegister;
 use EasySwoole\EasySwoole\AbstractInterface\Event;
 use EasySwoole\Http\Request;
@@ -28,8 +30,20 @@ class EasySwooleEvent implements Event
         CreateMysqlOrm::getInstance()->createMysqlOrm();
         CreateRedisPool::getInstance()->createRedis();
 
-        $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) {
-
+        /**
+         * **************** websocket控制器 **********************
+         */
+        // 创建一个 Dispatcher 配置
+        $conf = new \EasySwoole\Socket\Config();
+        // 设置 Dispatcher 为 WebSocket 模式
+        $conf->setType(\EasySwoole\Socket\Config::WEB_SOCKET);
+        // 设置解析器对象
+        $conf->setParser(new WebSocketParser());
+        // 创建 Dispatcher 对象 并注入 config 对象
+        $dispatch = new Dispatcher($conf);
+        // 给server 注册相关事件 在 WebSocket 模式下  on message 事件必须注册 并且交给 Dispatcher 对象处理
+        $register->set(EventRegister::onMessage, function (\swoole_websocket_server $server, \swoole_websocket_frame $frame) use ($dispatch) {
+            $dispatch->dispatch($server, $frame->data, $frame);
         });
     }
 
