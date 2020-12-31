27c80d267c068f48a59ec601f00cbcc40312d332
wanghan <minglongoc@me.com>
2020-12-31 10:21:09 +0800

    2020-12-31_10:21:09

--- App/Task/Docx2pdf.php
+++ App/Task/Docx2pdf.php
@@ -19,14 +19,11 @@ class Docx2pdf implements TaskInterface
     {
         $this->sendFile = array_filter($this->sendFile);
 
-        foreach ($this->sendFile as $key => $oneFile)
-        {
+        foreach ($this->sendFile as $key => $oneFile) {
             if (strpos($oneFile, 'docx') === false) continue;
-
             $this->sendFile[$key] = str_replace('docx', 'pdf', $oneFile);
-
             date('Ymd') == '20201231' ?
-                CommonService::getInstance(['docs2pdf'=>Carbon::now()->format('Y-m-d H:i:s')]) :
+                CommonService::getInstance(['docs2pdf' => Carbon::now()->format('Y-m-d H:i:s'), 'file' => $this->sendFile]) :
                 file_get_contents('http://127.0.0.1:8992/docx2pdf/' . $oneFile);
         }
 
