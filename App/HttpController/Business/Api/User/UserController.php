2e1a0256ffb4e24ab170d6f83cf1628ec4b8eb69
wanghan <minglongoc@me.com>
2021-01-14 18:11:20 +0800

    2021-01-14_18:11:20

--- App/HttpController/Business/Api/User/UserController.php
+++ App/HttpController/Business/Api/User/UserController.php
@@ -699,7 +699,9 @@ class UserController extends BusinessBase
         $res['handleStatus'] = $handleStatus;
         $res['handleTime'] = $handleTime;
         $res['errInfo'] = $orderInfo->errInfo;
-        $res['redirect'] = preg_match('/[协议|承诺]/u',$orderInfo->errInfo) ? '协议或承诺书' : '其他';
+        $cond1 = count(explode('协议',$orderInfo->errInfo)) >= 2;
+        $cond2 = count(explode('承诺',$orderInfo->errInfo)) >= 2;
+        ($cond1 || $cond2) ? $res['redirect'] = '协议或承诺书' : $res['redirect'] = '其他';
         $res['filePackage'] = $orderInfo->filePackage;
 
         return $this->writeJson(200,null,$res,'成功');
