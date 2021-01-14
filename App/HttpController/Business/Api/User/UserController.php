b55bd43fd5b40635b43c9f1c3fd3ee181d3d3343
wanghan <minglongoc@me.com>
2021-01-14 14:52:05 +0800

    2021-01-14_14:52:05

--- App/HttpController/Business/Api/User/UserController.php
+++ App/HttpController/Business/Api/User/UserController.php
@@ -613,7 +613,7 @@ class UserController extends BusinessBase
                 //签字盖章!
                 $docxObj->setImageValue('zhang', [
                     'path' => STATIC_PATH . 'mzjd_zhang_one.png',
-                    'width' => '',
+                    'width' => null,
                     'height' => 180
                 ]);
                 $docxObj->saveAs(FILE_PATH . $orderId . '.docx');
