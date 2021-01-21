32645a8f3c3e0fb5f3dbd9ae11eccc331aa06cf4
wanghan <minglongoc@me.com>
2021-01-21 20:20:52 +0800

    2021-01-21_20:20:52

--- App/Crontab/CrontabList/RunSupervisor.php
+++ App/Crontab/CrontabList/RunSupervisor.php
@@ -29,8 +29,7 @@ class RunSupervisor extends AbstractCronTask
     static function getRule(): string
     {
         //一月一次
-        //return '0 0 1 */1 *';
-        return '*/5 * * * *';
+        return '0 0 1 */1 *';
     }
 
     static function getTaskName(): string
