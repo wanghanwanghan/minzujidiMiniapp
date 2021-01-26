8aa9236c875c08528dc4c6c049e271930b45ce96
wanghan <minglongoc@me.com>
2021-01-26 11:15:14 +0800

    2021-01-26_11:15:14

--- App/HttpController/Service/ExportExcelService.php
+++ App/HttpController/Service/ExportExcelService.php
@@ -17,11 +17,21 @@ class ExportExcelService extends ServiceBase
     }
 
     //只填数据
-    function export(Excel $fileObject, $entList): bool
+    function export(Excel $fileObject, array $entList): bool
     {
         $entList = array_filter($entList);
 
         if (empty($entList)) return false;
+
+        $entListTemp = '';
+
+        foreach ($entList as $one)
+        {
+            $entList .= "'{$one}',";
+        }
+
+        $entListTemp = trim($entListTemp);
+        $entListTemp = trim($entListTemp,',');
 
         $header = [
             '订单号',
@@ -51,153 +61,17 @@ class ExportExcelService extends ServiceBase
             '经办人座机号',
             '经办人联系地址',
             '经办人电子邮箱',
-            '地址名称',
+            '住所',
             '租赁合同开始时间',
             '租赁合同到期时间',
             '租赁合同是否到期',
             '管理协议开始时间',
             '管理协议到期时间',
             '管理协议是否到期',
-            '民族园内申请单位',
+            '申请单位',
             '管理费',
             '应收管理费',
         ];
-
-        $list = Order::create()->alias('orderTable')
-            ->field([
-                'orderTable.orderId',
-                'entInfoTable.fileNumber',
-                'orderTable.entName',
-                'orderTable.phone',
-                'orderTable.userType',
-                'orderTable.taxType',
-                'orderTable.modifyAddr',
-                'orderTable.modifyArea',
-                'orderTable.areaFeeItems',
-                'orderTable.proxy',
-                'orderTable.finalPrice',
-                'orderTable.created_at',
-                'entInfoTable.code',
-                'entInfoTable.entStatusInApi',
-                'entInfoTable.entAddrInApi',
-                'entInfoTable.fr',
-                'entInfoTable.frCode',
-                'entInfoTable.frPhone',
-                'entInfoTable.frTel',
-                'entInfoTable.frAddr',
-                'entInfoTable.frEmail',
-                'entInfoTable.jbr',
-                'entInfoTable.jbrCode',
-                'entInfoTable.jbrPhone',
-                'entInfoTable.jbrTel',
-                'entInfoTable.jbrAddr',
-                'entInfoTable.jbrEmail',
-                'entInfoTable.addr',
-                'uploadTable.startTime',
-                'uploadTable.endTime',
-                '"" as zlhtIsExpire',
-                '"" as xyStartTime',
-                '"" as xyEndTime',
-                '"" as xyIsExpire',
-                'entInfoTable.applyEnt',
-                'entInfoTable.managementPrice',
-                'entInfoTable.receivableManagementPrice',
-            ])
-            ->join('miniapp_ent_info as entInfoTable', 'orderTable.orderId = entInfoTable.orderId', 'left')
-            ->join('miniapp_upload_file as uploadTable', 'orderTable.orderId = uploadTable.orderId', 'left')
-            ->where('orderTable.entName', $entList, 'in')
-            ->where([
-                'orderTable.status' => 3,
-                'orderTable.handleStatus' => 4,
-                'uploadTable.type' => 4,
-            ])
-            ->all();
-
-        $list = obj2Arr($list);
-
-        $tmp = [];
-
-        if (!empty($list)) {
-            foreach ($list as $one) {
-                switch ($one['userType']) {
-                    case 1:
-                        $one['userType'] = '民族园内企业';
-                        break;
-                    case 2:
-                        $one['userType'] = '新注册企业';
-                        break;
-                    case 3:
-                        $one['userType'] = '渠道方';
-                        break;
-                }
-
-                switch ($one['taxType']) {
-                    case 1:
-                        $one['taxType'] = '小规模纳税人';
-                        break;
-                    case 2:
-                        $one['taxType'] = '一般纳税人';
-                        break;
-                }
-
-                switch ($one['modifyAddr']) {
-                    case 1:
-                        $one['modifyAddr'] = '需要变更地址';
-                        break;
-                    case 2:
-                        $one['modifyAddr'] = '不需要变更地址';
-                        break;
-                }
-
-                switch ($one['modifyArea']) {
-                    case 1:
-                        $one['modifyArea'] = '不同区';
-                        break;
-                    case 2:
-                        $one['modifyArea'] = '同区';
-                        break;
-                }
-
-                $areaFeeItems = '';
-                if (strpos($one['areaFeeItems'], '1') !== false) $areaFeeItems .= '工商,';
-                if (strpos($one['areaFeeItems'], '2') !== false) $areaFeeItems .= '税务,';
-                if (strpos($one['areaFeeItems'], '3') !== false) $areaFeeItems .= '银行,';
-                if (strpos($one['areaFeeItems'], '4') !== false) $areaFeeItems .= '社保,';
-                if (strpos($one['areaFeeItems'], '5') !== false) $areaFeeItems .= '公积金,';
-                $one['areaFeeItems'] = trim($areaFeeItems, ',');
-
-                switch ($one['proxy']) {
-                    case 1:
-                        $one['proxy'] = '需要代理记账';
-                        break;
-                    case 2:
-                        $one['proxy'] = '不需要代理记账';
-                        break;
-                }
-
-                $one['created_at'] = date('Y-m-d H:i:s', $one['created_at']);
-
-                $one['startTime'] = date('Y-m-d', $one['startTime']);//upload type = 4 租赁合同时间
-                $one['endTime'] = date('Y-m-d', $one['endTime']);
-                if (!empty($one['endTime'])) {
-                    $one['zlhtIsExpire'] = time() > $one['endTime'] ? '已经过期' : '未过期';
-                }
-
-                //添加管理协议时间，如果存在
-                $xy = UploadFile::create()->where(['orderId' => $one['orderId'], 'type' => 6])->get();
-
-                if (!empty($xy)) {
-                    //
-                    $one['xyStartTime'] = date('Y-m-d', $xy['startTime']);
-                    $one['xyEndTime'] = date('Y-m-d', $xy['endTime']);
-                    if (!empty($xy['endTime'])) {
-                        $one['xyIsExpire'] = time() > $xy['endTime'] ? '已经过期' : '未过期';
-                    }
-                }
-
-                $tmp[] = array_values($one);
-            }
-        }
 
         //不用orm方式了
         $obj = Manager::getInstance()->get('miniapp')->getObj();
@@ -246,7 +120,7 @@ FROM
 	LEFT JOIN miniapp_ent_info AS entInfoTable ON orderTable.orderId = entInfoTable.orderId
 	LEFT JOIN miniapp_upload_file AS uploadTable ON orderTable.orderId = uploadTable.orderId 
 WHERE
-	`orderTable`.`entName` IN ( '芝麻云服（北京）信息技术有限公司' ) 
+	`orderTable`.`entName` IN ( {$entListTemp} ) 
 	AND `orderTable`.`status` = 3 
 	AND `orderTable`.`handleStatus` = 4 
 	AND `uploadTable`.`type` = 4
